645fa4d4d6fbcedac198539af3cbd0b916af6cef
Martin Hellebostad Andresen <martin.hellebostad.andresen@intility.no>
2022-10-05 17:15:04 +0200

    fix: logout svg size

--- components/ServerTab/index.tsx
+++ components/ServerTab/index.tsx
@@ -8,12 +8,12 @@ type Props = {
 
 export default function ServerTab({ server }: Props) {
 	return (
-		<div>
+		<div className="m-4">
 			{/* ADD SLUG */}
 			<Link href={`/`}>
 				<a
 					key={server.id}
-					className="flex justify-between p-3 bg-bfc-base-3 cursor-pointer no-underline rounded h-[140px] group border border-red-500"
+					className="flex justify-between p-3 bg-bfc-base-3 cursor-pointer no-underline rounded h-[140px] group border border-gray-500"
 				>
 					<h1>{server.name}</h1>
 					<button>{!server ? 'Configure' : 'Setup'}</button>
